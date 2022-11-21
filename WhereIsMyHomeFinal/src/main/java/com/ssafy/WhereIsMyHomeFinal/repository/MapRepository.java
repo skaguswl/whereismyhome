2452c70aa7158891d0a23cb30bfbbfbe0a165c6e
이지용 <dlwldyd9303@naver.com>
2022-11-21 13:44:15 +0900

    slkdfj

--- WhereIsMyHomeFinal/src/main/java/com/ssafy/WhereIsMyHomeFinal/repository/MapRepository.java
+++ WhereIsMyHomeFinal/src/main/java/com/ssafy/WhereIsMyHomeFinal/repository/MapRepository.java
@@ -10,10 +10,10 @@ import java.util.List;
 
 public interface MapRepository extends JpaRepository<Dongcode, Long> {
 
-    @Query("select new com.ssafy.WhereIsMyHomeFinal.domain.dto.MapDto(d.dongCode, d.sidoName) from Dongcode d where d.dongCode like '%00000000'")
+    @Query("select new com.ssafy.WhereIsMyHomeFinal.domain.dto.MapDto(substring(d.dongCode, 1, 2), d.sidoName) from Dongcode d where d.dongCode like '%00000000'")
     List<MapDto> findSido();
 
-    @Query("select new com.ssafy.WhereIsMyHomeFinal.domain.dto.MapDto(d.dongCode, d.gugunName) from Dongcode d where d.dongCode like concat(:regcodePattern,'__000000')")
+    @Query("select new com.ssafy.WhereIsMyHomeFinal.domain.dto.MapDto(substring(d.dongCode, 1, 4), d.gugunName) from Dongcode d where d.dongCode like concat(:regcodePattern,'__000000')")
     List<MapDto> findGugun(@Param("regcodePattern") String regcodePattern);
 
     @Query("select new com.ssafy.WhereIsMyHomeFinal.domain.dto.MapDto(d.dongCode, d.dongName) from Dongcode d where d.dongCode like concat(:regcodePattern,'%')")
